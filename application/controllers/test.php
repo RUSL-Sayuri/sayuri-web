7138c8eee3f1488160d852fffe125f085d75c851
Thejan Rajapakshe <coder.clix@gmail.com>
2015-06-21 23:40:12 +0530

    init

---
+++ application/controllers/test.php
@@ -0,0 +1,13 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Test extends CI_Controller
+{
+    public function test2($view)
+    {
+        $this->load->view($view);
+    }
+
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
